28974c099d2a8a5f705fee0f1ed923e18742e786
nomanareeb32 <nomanareeb32@gmail.com>
2022-06-09 15:04:43 +0600

    sort is added

---
+++ LEETCODE_find-target-indices-after-sorting-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        less_target = 0
+        n_target = 0
+        for value in nums:
+            if value < target:
+                less_target += 1
+            if value == target:
+                n_target += 1
+
+        result = [less_target + i for i in range(n_target)]
+        return result
