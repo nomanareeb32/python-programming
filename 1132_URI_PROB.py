e0145b8b3c27a26a535a7fc99500e7a2ed17c9af
nomanareeb32 <nomanareeb32@gmail.com>
2022-06-30 15:36:22 +0600

    hackerrank and uri prob is added

--- 1132_URI_PROB.py
+++ 1132_URI_PROB.py
@@ -1,7 +1,15 @@
 x = int(input())
 y = int(input())
 s = 0
-for i in range(x, y + 1):
-    if i % 13 != 0:
-        s += i
+if x < y:
+    for i in range(x, y + 1):
+        if i % 13 != 0:
+            s += i
+if y < x:
+    for i in range(y, x + 1):
+        if i % 13 != 0:
+            s += i
+if x == y:
+    if x % 13 != 0:
+        s = x
 print(s)
